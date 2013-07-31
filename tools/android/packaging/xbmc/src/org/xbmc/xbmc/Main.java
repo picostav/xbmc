8a980b5abffa46a0fd888e67c1e6f72a0878e647
Cory Fields <theuni-nospam-@xbmc.org>
2013-07-31 02:36:27 -0400

    droid: backport java and manifest changes

---
+++ tools/android/packaging/xbmc/src/org/xbmc/xbmc/Main.java
@@ -0,0 +1,38 @@
+package org.xbmc.xbmc;
+
+import android.app.NativeActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnSystemUiVisibilityChangeListener;
+
+public class Main extends NativeActivity implements View.OnSystemUiVisibilityChangeListener
+{
+  native void _onSystemUiVisibilityChange(int visibility);
+  native void _onNewIntent(Intent intent);
+  public Main() 
+  {
+    super();
+  }
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) 
+  {
+    super.onCreate(savedInstanceState);
+    getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(this);
+  }
+
+  @Override
+  protected void onNewIntent(Intent intent)
+  {
+    super.onNewIntent(intent);
+    _onNewIntent(intent);
+  }
+
+  @Override
+  public void onSystemUiVisibilityChange(int visibility)
+  {
+    _onSystemUiVisibilityChange(visibility);
+  }
+
+}
